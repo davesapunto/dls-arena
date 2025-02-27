8db87f476ffcb5506834fe3252b37509788390a6
John Dave Sapunto <118431829+davesapunto@users.noreply.github.com>
2025-02-27 16:43:57 +0800

    Ano to

---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function SignIn(){
+    return(
+      <div className = "w-[125vh] bg-black">
+        <div className = "border border-black border-1 w-96 h-96">
+            Home
+        </div>
+      </div>
+    );
+  }
